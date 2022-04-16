d688fd09abb966c229321c0b39026168b03b91cd
sina lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2022-04-16 22:11:00 +0430

    حفظ آدرس های مزاحم اول ترمینال
    For annoying addresses first in terminall VS Code
    این تابع قراره از نوشتن 2 خط جلوگیری کنه و اون رو داخل 1 خط خلاصه کنه !!! 
    البته از یک برنامه نویس بیگینر Beginner 
    نمیشه توقع بیشتری داشته باشی

---
+++ 37- def introduction ver19.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+def import_os():
+    """ این تابع برای این هست که دیگه دو خط پایین رو ننویسی
+     this function help lazy man :)))) 
+     so you can use this :)))
+     """
+    import os
+    os.system('cls')
+
+import_os()
+
