beb23a879bbe37d49fb8f9c27f8d5b1e83597a54
sina lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2022-04-16 23:17:13 +0430

    Global Scope and Local Scope and Scope Resolution
    Global Scope and Local Scope and Scope Resolution
    this is gift from python to us :)))))

---
+++ 39- def introduction ver21.py
@@ -0,0 +1,23 @@
+def import_os():
+    """ این تابع برای این هست که دیگه دو ایمپورت و دات سیستم رو ننویسی
+     this function help lazy man :)))) 
+     so you can use this :)))
+     """
+    import os
+    os.system('cls')
+
+import_os()
+
+
+def returner_a():
+    a = 10
+    def receiver_a():
+        return a
+    return receiver_a()
+
+
+print(returner_a())
+
+
+
+
